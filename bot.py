702d2f269d99e26e77655c44ec39ec780fb14add
Forlene <aimawaforlene@gmail.com>
2026-04-26 18:17:34 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,15 @@
+import os  
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
-
-TOKEN = "8592484270:AAEJEp4IY966ZYAUtDQqNtwCoXCiqgqOhJE"
+TOKEN = os.getenv("BOT_TOKEN :8592484270:AAEJEp4IY966ZYAUtDQqNtwCoXCiqgqOhJE") 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bonjour 👋 Je suis votre chatbot.")
-
-async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
-   if update.message and update.message:
-       user_message = update.message.text.lower()
-
-    if "bonjour" in user_message:
-        response = "Bonjour 😊 Comment puis-je vous aider ?"
-    elif "prix" in user_message:
-        response = "Voici notre grille tarifaire."
+if __name__ == '__main__':
+    if not TOKEN:
+        print("Erreur : Le token BOT_TOKEN n'est pas défini.")
     else:
-        response = "Je n’ai pas compris votre demande."
-
-    await update.message.reply_text(response)
-
-app = ApplicationBuilder().token(TOKEN).build()
-
-app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
-
-app.run_polling()
+        app = ApplicationBuilder().token(TOKEN).build()
+        app.add_handler(CommandHandler("start", start))
+        app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
+        app.run_polling()
