0793b07577ceb9380707189ab4ccaa10cd69830e
Forlene <aimawaforlene@gmail.com>
2026-04-26 15:55:48 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bonjour 👋 Je suis votre chatbot.")
 
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_message = update.message.text.lower()
+   if update.message and update.message:
+       user_message = update.message.text.lower()
 
     if "bonjour" in user_message:
         response = "Bonjour 😊 Comment puis-je vous aider ?"
