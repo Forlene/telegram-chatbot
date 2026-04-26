84a2d99a310ad0124987be1fba4610a5baf541f7
Forlene <aimawaforlene@gmail.com>
2026-04-26 18:22:20 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,18 @@ TOKEN = os.getenv("BOT_TOKEN :8592484270:AAEJEp4IY966ZYAUtDQqNtwCoXCiqgqOhJE")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bonjour 👋 Je suis votre chatbot.")
+async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message and update.message.text:
+        user_message = update.message.text.lower()
+
+        if "bonjour" in user_message:
+            response = "Bonjour 👋 Comment puis-je vous aider ?"
+        elif "prix" in user_message:
+            response = "Voici notre grille tarifaire."
+        else:
+            response = "Je n’ai pas compris votre demande."
+
+        await update.message.reply_text(response)
 if __name__ == '__main__':
     if not TOKEN:
         print("Erreur : Le token BOT_TOKEN n'est pas défini.")
